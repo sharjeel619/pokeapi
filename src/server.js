c5f9e64b6e744761c52b9be16fe85f2592e32771
Sharjeel Imtiaz <imtiazsharjeel@gmail.com>
2020-12-18 07:37:16 +0500

    Change express.js paths

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(__dirname, `build/index.html`))
 });
 app.listen(port, () => {
    console.log('Server is running!');
